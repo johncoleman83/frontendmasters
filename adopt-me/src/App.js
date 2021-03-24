91e5ac95dbcabbefedb0b82c84162b716eef3b7d
David John Coleman II <jcoleman@pagerduty.com>
2021-03-24 09:31:17 -0500

    add useState and modified search form

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams"
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <SearchParams />
+      {/* <Pet name="Luna" animal="dog" breed="Havanese" />
       <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      <Pet name="Doink" animal="cat" breed="Mix" /> */}
     </div>
   );
 };
--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet"
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input id="location"
+          value={location}
+          placeholder="Location"
+          onChange={e => setLocation(e.target.value)}/>
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}>
+            <option>ALL</option>
+            {ANIMALS.map(a => (
+              <option key={a} value={a}>{a}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
