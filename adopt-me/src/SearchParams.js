ad7f5aa1ac0d0d710d09934135571be7f59bccc2
David John Coleman II <jcoleman@pagerduty.com>
2021-03-24 14:55:51 -0500

    more search options

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,13 +1,46 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet"
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet"
+import useDropdown from "./useDropdown"
+import Results from "./Results"
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
-  const [animal, setAnimal] = useState("dog");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals(
+      {
+        location,
+        breed,
+        type: animal
+      }
+    )
+    console.log("animals", animals);
+
+    setPets(animals || []);
+  }
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrings = apiBreeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input id="location"
@@ -15,20 +48,11 @@ const SearchParams = () => {
           placeholder="Location"
           onChange={e => setLocation(e.target.value)}/>
         </label>
-        <label htmlFor="animal">
-          Animal
-          <select id="animal"
-            value={animal}
-            onChange={e => setAnimal(e.target.value)}
-            onBlur={e => setAnimal(e.target.value)}>
-            <option>ALL</option>
-            {ANIMALS.map(a => (
-              <option key={a} value={a}>{a}</option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown/>
+        <BreedDropdown/>
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
