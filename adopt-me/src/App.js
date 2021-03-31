3e96c5b83881e2c435c2f6f383de908c3b4b4bea
David John Coleman II <jcoleman@pagerduty.com>
2021-03-31 15:33:28 -0500

    added Details class

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -2,16 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import Pet from "./Pet";
 import SearchParams from "./SearchParams"
+import { Router, Link } from "@reach/router"
+import Details from "./Details"
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-      {/* <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" /> */}
-    </div>
+    <React.StrictMode>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
   );
 };
 
